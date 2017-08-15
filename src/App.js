e75b3d6643463074513d3dae7f315502b5617e8d
rkiel <cygnus@pobox.com>
2017-08-15 17:56:25 -0400

    episode-3: added simple ul tag

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ul>
+          <li className="girl">Name 1 </li>
+          <li className="boy">Name 2 </li>
+          <li className="girl">Name 3 </li>
+          <li className="boy">Name 4 </li>
+          <li className="girl">Name 5 </li>
+          <li className="boy">Name 6 </li>
+        </ul>
       </div>
     );
   }
