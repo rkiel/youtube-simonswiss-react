31c72bc17f2f0a70698bf4ef7144916b89838f8a
rkiel <cygnus@pobox.com>
2017-08-15 21:07:30 -0400

    episode-7: added Credits component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './App.css';
+
 import NamesList from './components/NamesList';
+import Credits from './components/Credits';
 
 class App extends Component {
   render() {
     return (
       <div>
         <NamesList data={this.props.data} />
+        <Credits />
       </div>
     );
   }
---
+++ src/components/Credits.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Credits = () =>
+  <div>
+    <p>
+      Source of names list:{' '}
+      <a href="https://www.yahoo.com/news/atticus-tops-baby-names-2015-124073377716.html">
+        Yahoo - Top Baby Names in 2015
+      </a>
+    </p>
+  </div>;
+
+export default Credits;
