91198ed8b3e75e039ae611938a922aa2341195f2
rkiel <cygnus@pobox.com>
2017-08-15 20:44:40 -0400

    episode-5: use data as props

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import './App.css';
 
 class App extends Component {
   render() {
+    const { data } = this.props;
+    const namesList = data.map(datum =>
+      <li key={datum.id} className={datum.sex}>
+        {datum.name}
+      </li>
+    );
     return (
       <div className="App">
         <ul>
-          <li className="girl">Name 1 </li>
-          <li className="boy">Name 2 </li>
-          <li className="girl">Name 3 </li>
-          <li className="boy">Name 4 </li>
-          <li className="girl">Name 5 </li>
-          <li className="boy">Name 6 </li>
+          {namesList}
         </ul>
       </div>
     );
   }
 }
-
+App.propTypes = {
+  data: PropTypes.arrayOf(PropTypes.object).isRequired
+};
 export default App;
